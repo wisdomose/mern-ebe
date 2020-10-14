a33d0210f3da63f4dbb3cd949db0567108372c9f
wisdomose <35114555+wisdomose@users.noreply.github.com>
2020-10-14 22:51:33 +0100

    Add files via upload

---
+++ precache-manifest.b65ad3ec7af3b2973d3ea8b1e0ccc0ed.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c5a64ec4e82bf962ec816078a04080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "951e576a5fd8cee529e0",
+    "url": "/static/css/main.b37bce16.chunk.css"
+  },
+  {
+    "revision": "2ef123ae878daed13e51",
+    "url": "/static/js/2.fb2696f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fb2696f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951e576a5fd8cee529e0",
+    "url": "/static/js/main.3d10b8d1.chunk.js"
+  },
+  {
+    "revision": "675e13c9c0db9e25e165",
+    "url": "/static/js/runtime-main.f1adceea.js"
+  },
+  {
+    "revision": "c41b510d20d3b1f8c1de49cec755cdcc",
+    "url": "/static/media/001.c41b510d.png"
+  },
+  {
+    "revision": "1b5217b8e1461ddda5b1396aa1826732",
+    "url": "/static/media/creche.1b5217b8.jpg"
+  },
+  {
+    "revision": "b365d552bfd003034a113d493ce65e12",
+    "url": "/static/media/hero-img.b365d552.jpg"
+  },
+  {
+    "revision": "1af7781a66f262728a8b305b1584dd12",
+    "url": "/static/media/home.1af7781a.svg"
+  },
+  {
+    "revision": "6c69b59d98211c89ff2e88ed3bf9c816",
+    "url": "/static/media/menu.6c69b59d.svg"
+  },
+  {
+    "revision": "b14562456019e74e3c10d913e1f2ff6b",
+    "url": "/static/media/nursery.b1456245.jpg"
+  },
+  {
+    "revision": "e854fe635f95408768e189e3370d2972",
+    "url": "/static/media/phone.e854fe63.svg"
+  },
+  {
+    "revision": "f2dc72d95fba11ff5018f333673c0cf9",
+    "url": "/static/media/primary.f2dc72d9.jpg"
+  },
+  {
+    "revision": "b75a6782340765a158a567de9703add2",
+    "url": "/static/media/question.b75a6782.svg"
+  },
+  {
+    "revision": "6410337b0dff52e1d32f8b51d1785e91",
+    "url": "/static/media/secondary.6410337b.jpg"
+  },
+  {
+    "revision": "379581a1f26d024a73c753c41844db8d",
+    "url": "/static/media/sign-in.379581a1.svg"
+  }
+]);
